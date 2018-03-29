af58abcdee633d08d5468ec631ce19b09f41cc11
capitek-wangsj <wangsj@capitek.com.cn>
2018-03-29 17:42:43 +0800

    方法封装为Worker类

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
+# coding=utf-8
 import argparse
+import json
+import os
+import time
 
+import pika
 import potsdb
 
 import settings
-from utils.mq_client import MQClient
-from utils.opentsdb_client import OpentsdbClient
-from publisher import Publisher
-from consumer import Consumer
+from utils.util_logs import read_log_file, read_log_dir
 
 parser = argparse.ArgumentParser(description='provide a role name')
 parser.add_argument('--role', type=str, default=None)
@@ -17,28 +19,103 @@ CONSUMER = 'consumer'
 ROLES = (PUBLISHER, CONSUMER)
 
 
+class Worker:
+    def __init__(self, role):
+        self.role = role
+        if self.role not in ROLES:
+            raise Exception("Invalid role: %s" % self.role)
+
+        self.db_client = self.get_db_client()
+        self.connection, self.channel = self.get_mq_channel()
+
+    def start(self):
+        print "The {} is running...".format(args.role)
+        if self.role == PUBLISHER:
+            self.publisher()
+        elif self.role == CONSUMER:
+            self.consumer()
+        else:
+            return
+
+    def get_db_client(self):
+        return potsdb.Client(host=settings.OPENTSDB_HOST, port=settings.OPENTSDB_PORT,
+                             qsize=1000, host_tag=True,
+                             mps=100, check_host=True)
+
+    def get_mq_channel(self):
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host=settings.MQ_HOST))  # 创建一个连接
+        channel = connection.channel()  # 创建通道
+        channel.queue_declare(queue=settings.MQ_QUEUE, durable=True)  # 声明 queue
+        return connection, channel
+
+    def publisher(self):
+        connection, channel = self.get_mq_channel()
+        try:
+            while 1:
+                latest_log_name = self.get_latest_log_name()
+                log_filenames = read_log_dir(settings.LOG_PATH, latest_log_name)
+                channel.basic_publish(exchange=settings.MQ_EXCHANGE,
+                                      routing_key=settings.MQ_ROUTING_KEY,
+                                      body=json.dumps(log_filenames))
+                time.sleep(settings.SLEEP_TIME)
+
+        except Exception as ex:
+            print ex.message
+        finally:
+            connection.close()
+
+    def consumer(self):
+        connection, channel = self.get_mq_channel()
+        try:
+            channel.basic_consume(self.callback, queue=settings.MQ_QUEUE, no_ack=True)
+            print(' [*] Waiting for messages. To exit press CTRL+C')
+            channel.start_consuming()  # 创建死循环，监听消息队列
+        except Exception as ex:
+            print ex.message
+        finally:
+            connection.close()
+
+    def get_latest_log_name(self):
+        # todo get the latest_log_name from one place
+        return ""
+
+    def save_latest_log_name(self, latest_log_name):
+        # todo save the latest_log_name in one place
+        pass
+
+    # 回调函数中收到通知后，做相关操作： 解析body，保存存档点，循环读取日志内容，把日志内容写入数据库
+    def callback(self, ch, method, properties, body):
+        print 'ch: %s; method: %s; properties: %s; body: %s;' % (ch, method, properties, body)
+
+        try:
+            log_names = json.loads(body)
+            if log_names:
+                latest_log_name = log_names[0]
+                self.save_latest_log_name(latest_log_name)
+                for i, log_name in enumerate(log_names):
+                    print '%d: Reading the file: %s' % (i, log_name)
+                    logs = read_log_file(os.path.join(settings.LOG_PATH, log_name))
+                    if logs:
+                        for j, log in enumerate(logs):
+                            if isinstance(log, dict):
+                                print '--- %d.%d: Reading the line' % (i, j)
+                                # self.db_client.log('log.metric', 100, timestamp=log.get('event_timestamp'), **log)
+                            else:
+                                print '!!! %d.%d: The content of this line [%s] is not invalid' % \
+                                      (i, j, log)
+            else:
+                print "The content of this log is empty." % body
+
+        except Exception as ex:
+            print ex.message
+            print "!!! the format of the body is invalid: %s" % body
+        finally:
+            pass
+
+
 def main():
-    if not args.role or args.role not in ROLES:
-        print "please provide a role name: {}".format(ROLES)
-        return
-
-    print "The {} is running...".format(args.role)
-
-    mqClient = MQClient(settings.MQ_ADDRESS, settings.MQ_USER, settings.MQ_PASSWORD, settings.MQ_EXCHANGE,
-                        settings.MQ_QUEUE)
-    dbClient = potsdb.Client(host=settings.OPENTSDB_HOST, port=settings.OPENTSDB_HOST, qsize=1000, host_tag=True, mps=100, check_host=True)
-
-    # metrics.send('devbox.metric5', 100, extratag1='tagvalue', extratag2='tagvalue')
-    # metrics.wait()
-
-    if args.role == PUBLISHER:
-        pub = Publisher(mqClient, settings.LOG_PATH, settings.SLEEP_TIME)
-        pub.start()
-    elif args.role == CONSUMER:
-        con = Consumer(mqClient, dbClient)
-        con.start()
-    else:
-        return
+    worker = Worker(args.role)
+    worker.start()
 
 
 if __name__ == "__main__":
--- settings.py
+++ settings.py
@@ -1,13 +1,13 @@
 # -*- coding: UTF-8 -*-
 
 LOG_PATH = '/usr/local/capitek/aaa/data/server/log/radacct/acctrec/'
-SLEEP_TIME = 2
 
-MQ_ADDRESS = 'xx:xx:xx:xx'
-MQ_USER = 'admin'
-MQ_PASSWORD = '123456'
-MQ_EXCHANGE = '123456'
-MQ_QUEUE = '123456'
+SLEEP_TIME = 10
 
-OPENTSDB_HOST = '127.0.0.1'
+OPENTSDB_HOST = 'localhost'
 OPENTSDB_PORT = 4242
+
+MQ_HOST = 'localhost'
+MQ_EXCHANGE = 'exchange_logs'
+MQ_QUEUE = 'queue_logs'
+MQ_ROUTING_KEY = 'routing_logs'
